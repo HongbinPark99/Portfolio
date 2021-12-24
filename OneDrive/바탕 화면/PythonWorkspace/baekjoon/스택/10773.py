c22f2fde82a92c26f150d68fd282e72c1f066a85
hongbinpark99 <hongbin542@gmail.com>
2021-12-24 13:09:02 +0900

    stack

---
+++ OneDrive/바탕 화면/PythonWorkspace/baekjoon/스택/10773.py
@@ -0,0 +1,11 @@
+import sys
+k = int(sys.stdin.readline())
+stack = []
+
+for i in range(k):
+    num = int(sys.stdin.readline())
+    if num != 0:
+        stack.append(num)
+    else:
+        stack.pop()
+print(sum(stack))
