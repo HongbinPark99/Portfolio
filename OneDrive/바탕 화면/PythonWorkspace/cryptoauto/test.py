57611b31af6e1e2291559dedf5fc010bf7031592
HongbinPark99 <89118596+HongbinPark99@users.noreply.github.com>
2021-12-30 12:44:56 +0900

    Update test.py

--- OneDrive/바탕 화면/PythonWorkspace/cryptoauto/test.py
+++ OneDrive/바탕 화면/PythonWorkspace/cryptoauto/test.py
@@ -4,6 +4,6 @@ access = ""          # 본인 값으로 변경
 secret = ""          # 본인 값으로 변경
 upbit = pyupbit.Upbit(access, secret)
 
-print(upbit.get_balance("KRW-BTC"))     # KRW-XRP 조회
+print(upbit.get_balance("KRW-BTC"))     # KRW-BTC 조회
 print(upbit.get_balance("KRW"))         # 보유 현금 조회
 print(pyupbit.get_tickers("KRW"))
